1098fb00e64600ea2ce7a88e3d929a9dc60d83a6
davidpaelml <david.pael@mlhuillier.com>
2023-01-21 13:16:57 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"; // eslint-disable-next-line
+import { useEffect, useMemo, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [location, setLocation] = useState({
+    latitude: null,
+    longitude: null,
+  });
+
+  useEffect(() => {
+    (async () => {
+      let geolocation = await navigator.geolocation;
+      if (!geolocation) {
+        console.log("Permission to access location was denied");
+        return;
+      }
+      await geolocation.watchPosition((position) => {
+        setLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+      });
+    })();
+  }, []);
+
+  const showMapCOntainer = useMemo(() => {
+    if (location.latitude && location.longitude) {
+      return (
+        <MapContainer
+          center={[location.latitude, location.longitude]}
+          zoom={20}
+          scrollWheelZoom={false}
+          style={{ height: "100vh", width: "100vw" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker
+            position={[location.latitude, location.longitude]}
+            draggable={true}
+          >
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      );
+    }
+
+    return (
+      <div style={{ height: "100vh", width: "100vw" }}>
+        <h1>Loading...</h1>
+      </div>
+    );
+  });
+
+  return <div>{showMapCOntainer}</div>;
 }
 
 export default App;
