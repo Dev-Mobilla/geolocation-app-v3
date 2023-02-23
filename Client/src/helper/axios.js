7c0e5a86c3ee71d19ea3251a99c9b22ea934b21d
dev-mobilla <101626428+Dev-Mobilla@users.noreply.github.com>
2023-02-23 08:28:38 +0800

    persist modal

--- Client/src/helper/axios.js
+++ Client/src/helper/axios.js
@@ -3,5 +3,5 @@ import axios from "axios";
 export default axios.create({
     // baseURL: "http://10.4.12.205:3000"
     // baseURL: "http://10.4.8.168:3000"
-    baseURL: "http://127.0.0.1:8000/"
+    baseURL: "https://branchlocator.onrender.com"
 })
