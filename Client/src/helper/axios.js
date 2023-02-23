2dc5ccba08b9f7ebab6d58a56deb5356722639a2
dev-mobilla <101626428+Dev-Mobilla@users.noreply.github.com>
2023-02-23 08:52:12 +0800

    Update axios.js

--- Client/src/helper/axios.js
+++ Client/src/helper/axios.js
@@ -3,5 +3,6 @@ import axios from "axios";
 export default axios.create({
     // baseURL: "http://10.4.12.205:3000"
     // baseURL: "http://10.4.8.168:3000"
-    baseURL: "https://branchlocator.onrender.com"
+    //baseURL: "https://branchlocator.onrender.com"
+    baseURL: "https://branchlocator.mlhuillier.net"
 })
