513b33f7ddf11de9c7b75e55896af16a3f6bb21f
dev-mobilla <101626428+Dev-Mobilla@users.noreply.github.com>
2023-06-08 18:08:29 +0800

    modify app MiHcm Confirmation

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import AppRouter from "./ROUTER/Approuter";
+import MIHCM from "./views/MIHCM";
 
 function App() {
 
   return (
     <>
-      <AppRouter/>
+      {/* <AppRouter/> */}
+      <MIHCM/>
     </>
   );
 }
